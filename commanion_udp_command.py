08631346e62e366d4156c30d7e4a5046f3edb55b
JamboPE <github@jdpe.co.uk>
2023-10-15 17:55:40 +0100

    Added logic
    added buzzer.py
    
    Added logic

--- commanion_udp_command.py
+++ commanion_udp_command.py
@@ -1,10 +1,7 @@
 import socket
-UDP_IP = "144.32.210.197"
-UDP_PORT = 16759
-MESSAGE = "BANK_PRESS 3 3"
+from config import UDP_IP, UDP_PORT, MESSAGE
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
 print("message:", MESSAGE)
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
