590f962b1cecf4b4710edf6216d2676522cf0634
JamboPE <github@jdpe.co.uk>
2023-10-15 18:14:24 +0100

    Added installation to setup-pi.sh
    Added apt update
    
    Fixed install bugs
    
    fixed RPi.GPIO installation

--- setup-pi.sh
+++ setup-pi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "Updating repositories"
+sudo apt-get update 
 echo "Installing depedencies"
-sudo apt-get install -y python-rpi.gpio python3-rpi.gpio netcat
+sudo apt install -y netcat-traditional python3 pip python3-RPi.GPIO
 echo " "
 echo "Edit the config.py file with the details of the companion server then run __main__.py"
