1ef6051c24b6a67142ee1df705a61f018ba0f8fe
JamboPE <github@jdpe.co.uk>
2023-10-15 18:10:47 +0100

    Added logic

---
+++ setup-pi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Installing depedencies"
+sudo apt-get install -y python-rpi.gpio python3-rpi.gpio netcat
+echo " "
+echo "Edit the config.py file with the details of the companion server then run __main__.py"
