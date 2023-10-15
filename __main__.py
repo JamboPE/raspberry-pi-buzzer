a377c3c64f4382da6bf5af1d8028dc8c4fd6776b
JamboPE <github@jdpe.co.uk>
2023-10-15 18:24:09 +0100

    Added Pi pin to config.py

--- __main__.py
+++ __main__.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from config import Pi_PIN
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(Pi_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 while True: # Run forever
-    if GPIO.input(10) == GPIO.HIGH:
+    if GPIO.input(pi_PIN) == GPIO.HIGH:
         print("Button pushed")
         import commanion_udp_command
