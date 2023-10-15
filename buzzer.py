474583d778913a4f5ba84c59012f0b1763c85ff8
JamboPE <github@jdpe.co.uk>
2023-10-15 17:55:40 +0100

    added buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -1,7 +1,7 @@
 import socket
 UDP_IP = "144.32.210.197"
 UDP_PORT = 16759
-MESSAGE = "BANK_PRESS 3 3"
+MESSAGE = "BANK-PRESS 96 11"
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
 print("message:", MESSAGE)
